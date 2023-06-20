3cc49941f692626fa2de6769e209f341423a3382
JeanAranda <jarandar2014@gmail.com>
2023-06-20 03:32:42 -0500

    Mi primer commit falta lo de los objetos

---
+++ src/01-variables-const.js
@@ -0,0 +1,10 @@
+let nombre = "Jean Pierre";
+nombre = "Annie";
+const apellido = "Begazo";
+const condicion = true;
+if (condicion) {
+    nombre = "Piero";
+    const apellido = "Beltran";
+    console.log(nombre+ " "+apellido);
+}
+console.log(`Hola ${nombre} ${apellido} eres una gran persona tu puedes`);
---
+++ src/02-function.js
@@ -0,0 +1,9 @@
+
+
+const decirHola = (nombre = 'Pierre', edad = 50) => 
+`Hola mundo en la function ${nombre} tiene ${edad}`;
+const suma = (a = 0, b = 0) => a + b;
+const result = decirHola();
+
+console.log(result);
+console.log(15 + 5);
